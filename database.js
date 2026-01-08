97a5d24f947635056eca4eed41832058378d12f2
Francisco Jurado Galvez <fr.jurado@gmail.com>
2026-01-08 15:32:16 +0100

    Upload files

---
+++ database.js
@@ -0,0 +1,242 @@
+const database = [
+    {
+        "id": 1,
+        "frontImg": "./svg-final/135M_F_major1.svg",
+        "backImg": "./svg-final/135M_F_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 2,
+        "frontImg": "./svg-final/135M_Gb_major1.svg",
+        "backImg": "./svg-final/135M_Gb_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 3,
+        "frontImg": "./svg-final/135M_G_major1.svg",
+        "backImg": "./svg-final/135M_G_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 4,
+        "frontImg": "./svg-final/135M_Ab_major1.svg",
+        "backImg": "./svg-final/135M_Ab_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 5,
+        "frontImg": "./svg-final/135M_A_major1.svg",
+        "backImg": "./svg-final/135M_A_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 6,
+        "frontImg": "./svg-final/135M_Bb_major1.svg",
+        "backImg": "./svg-final/135M_Bb_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 7,
+        "frontImg": "./svg-final/135M_B_major1.svg",
+        "backImg": "./svg-final/135M_B_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 8,
+        "frontImg": "./svg-final/135M_C_major1.svg",
+        "backImg": "./svg-final/135M_C_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 9,
+        "frontImg": "./svg-final/135M_Db_major1.svg",
+        "backImg": "./svg-final/135M_Db_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 10,
+        "frontImg": "./svg-final/135M_D_major1.svg",
+        "backImg": "./svg-final/135M_D_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 11,
+        "frontImg": "./svg-final/135M_Eb_major1.svg",
+        "backImg": "./svg-final/135M_Eb_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 12,
+        "frontImg": "./svg-final/135M_E_major1.svg",
+        "backImg": "./svg-final/135M_E_major2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 13,
+        "frontImg": "./svg-final/135m_F_minor1.svg",
+        "backImg": "./svg-final/135m_F_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 14,
+        "frontImg": "./svg-final/135m_F#_minor1.svg",
+        "backImg": "./svg-final/135m_F#_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 15,
+        "frontImg": "./svg-final/135m_G_minor1.svg",
+        "backImg": "./svg-final/135m_G_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 16,
+        "frontImg": "./svg-final/135m_G#_minor1.svg",
+        "backImg": "./svg-final/135m_G#_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 17,
+        "frontImg": "./svg-final/135m_A_minor1.svg",
+        "backImg": "./svg-final/135m_A_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 18,
+        "frontImg": "./svg-final/135m_Bb_minor1.svg",
+        "backImg": "./svg-final/135m_Bb_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 19,
+        "frontImg": "./svg-final/135m_B_minor1.svg",
+        "backImg": "./svg-final/135m_B_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 20,
+        "frontImg": "./svg-final/135m_C_minor1.svg",
+        "backImg": "./svg-final/135m_C_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 21,
+        "frontImg": "./svg-final/135m_C#_minor1.svg",
+        "backImg": "./svg-final/135m_C#_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 22,
+        "frontImg": "./svg-final/135m_D_minor1.svg",
+        "backImg": "./svg-final/135m_D_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 23,
+        "frontImg": "./svg-final/135m_Eb_minor1.svg",
+        "backImg": "./svg-final/135m_Eb_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    },
+    {
+        "id": 24,
+        "frontImg": "./svg-final/135m_E_minor1.svg",
+        "backImg": "./svg-final/135m_E_minor2.svg",
+        "tags": [
+            "Lo básico",
+            "I & V",
+            "Do Mi Sol"
+        ]
+    }
+];
